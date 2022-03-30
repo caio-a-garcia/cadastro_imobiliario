9157f018b05e48a017c915c703e7615b9da4656d
Caio Garcia <caio.augusto@algar.com.br>
2022-03-29 23:10:26 -0300

    Implementa cadastro e metodos para retornar mapa com resumo dos objetos

--- __main__.py
+++ __main__.py
@@ -1,9 +1,15 @@
 #! python3
 # cadastro_imobiliario/__init__.py
 
-# import model
+from .model import Imovel, Individuo, Aluguel
 from hashlib import sha512
 from getpass import getpass
+from pprint import pprint
+
+imoveis = []
+inquilinos = []
+proprietarios = []
+alugueis = []
 
 def login():
     usuario = 'caio.crud'
@@ -16,38 +22,120 @@ def login():
 
     return autenticado
 
+
+def cadastro(tipo):
+    
+    def cadastrar_imovel():
+        cep = input('CEP: ')
+        cidade = input('Cidade: ')
+        bairro = input('Bairro: ')
+        logradouro = input('Logradouro: ')
+        id = len(imoveis)
+        return Imovel(logradouro, cep, bairro, cidade, id)
+
+    def cadastrar_individuo(grupo):
+        nome = input('Nome: ')
+        nascimento = input('Data de nascimento: ')
+        id = len(grupo)
+        return Individuo(nome, nascimento, id)
+
+#    def cadastrar_aluguel():
+        
+
+    if tipo == 'imovel':
+        imovel = cadastrar_imovel()
+        imoveis.append(imovel.resumo())
+    elif tipo == 'inquilino':
+        inquilino = cadastrar_individuo(inquilinos)
+        inquilinos.append(inquilino.resumo())
+    elif tipo == 'proprietario':
+        proprietario = cadastrar_individuo(proprietarios)
+        proprietarios.append(proprietario.resumo())
+    elif tipo == 'aluguel':
+        print('Cadastro de aluguel ainda nao implementado')
+    else:
+        raise ValueError('Argumento nao suportado por funcao cadastro(tipo)')
+
+
+def menu_cadastro():
+    enunciado = '''
+O que voce gostaria de cadastrar: 
+1) Imovel
+2) Inquilino
+3) Proprietario
+4) Aluguel
+0) Sair
+'''
+    tipo_cadastro = ''
+    sair = 0
+    repetir = ''
+
+    while True:
+        opcao = input(enunciado)
+        if opcao == '0':
+            sair = 1
+            break
+        elif opcao == '1':
+            tipo_cadastro = 'imovel'
+        elif opcao == '2':
+            tipo_cadastro = 'inquilino'
+        elif opcao == '3':
+            tipo_cadastro = 'proprietario'
+        elif opcao == '4':
+            tipo_cadastro = 'aluguel'
+        else:
+            print('Opcao invalida. Selecione o numero correspondente a sua opcao.')
+            continue
+        break
+
+    if sair != 1:
+        cadastro(tipo_cadastro)
+        repetir = input('Cadastrar mais um item? ')
+    if repetir == 'Sim':
+        menu_cadastro()
+
+
+def menu_consulta():
+    for imovel in imoveis:
+        pprint(imovel)
+
+    for inquilino in inquilinos:
+        pprint(inquilino)
+
+    for proprietario in proprietarios:
+        pprint(proprietario)
+
+
 def menu():
-    enunciado = '''Para escolher o que quer fazer, digite o numero relacionado a sua opcao:
+    enunciado = '''
+Para escolher o que quer fazer, digite o numero relacionado a sua opcao:
 1) Cadastro
 2) Consulta
 3) Atualizar
 4) Apagar
 0) Sair
 '''
+    
     while True:
         opcao = input(enunciado)
         if opcao == '0':
             print('Ate a proxima')
             break
         elif opcao == '1':
-            print('menu_cadastro()')
+            menu_cadastro()
         elif opcao == '2':
-            print('menu_consulta()')
+            menu_consulta()
         elif opcao == '3':
             print('menu_atualizar()')
         elif opcao == '4':
             print('menu_apagar()')
         else:
             print('Favor escolher uma opcao valida.')
-    
+
+    print(f'Imoveis: {imoveis}, Inquilinos: {inquilinos}, Proprietarios: {proprietarios}, Alugueis: {alugueis}')
     
 
 def start():
-    imoveis = []
-    inquilinos = []
-    proprietarios = []
-    alugueis = []
-
     while not login():
         print('Falha na autenticacao')
     print('Login bem sucedido')
--- model.py
+++ model.py
@@ -4,28 +4,56 @@
 from secrets import choice
 from string import ascii_letters, digits
 
-def generateID():
-    return ''.join(choice(ascii_letters + digits) for _ in range(16))
 
-class Imovel():
-    def __init__(self, logradouro, cep, bairro, cidade):
+def generateID():
+    return "".join(choice(ascii_letters + digits) for _ in range(16))
+
+
+class Imovel:
+    def __init__(self, logradouro, cep, bairro, cidade, id):
         self.logradouro = logradouro
         self.cep = cep
         self.bairro = bairro
         self.cidade = cidade
-        self.id = generateID()
+        self.id = id
+
+    def resumo(self):
+        mapa = {
+            "logradouro": self.logradouro,
+            "cep": self.cep,
+            "bairro": self.bairro,
+            "cidade": self.cidade,
+            "id": self.id,
+        }
+        return mapa
 
 
-class Individuo():
-    def __init__(self, nome, data_de_nascimento):
+class Individuo:
+    def __init__(self, nome, data_de_nascimento, id):
         self.nome = nome
         self.data_de_nascimento = data_de_nascimento
-        self.id = generateID()
+        self.id = id
+
+    def resumo(self):
+        mapa = {
+            "nome": self.nome,
+            "data_de_nascimento": self.data_de_nascimento,
+            "id": self.id,
+        }
+        return mapa
 
 
-class Aluguel():
-    def __init__(self, imovel, inquilino, proprietario):
-        self.imovel = imovel
-        self.inquilino = inquilino
-        self.proprietario = proprietario
-        self.id = generateID()
+# class Aluguel:
+#     def __init__(self, imovel_id, inquilino, proprietario, id):
+#         self.imovel = imovel_id
+#         self.inquilino_id = inquilino.id
+#         self.proprietario_id = proprietario.id
+#         self.id = id
+
+#     def resumo_ids(self):
+#         mapa = {
+#             "imovel": self.imovel,
+#             "inquilino": self.inquilino_id,
+#             "proprietario": self.proprietario_id,
+#             "aluguel_id": self.id,
+#         }
